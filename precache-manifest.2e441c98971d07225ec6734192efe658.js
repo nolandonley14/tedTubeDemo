98726598c0e8060cb984ed1205a8583aafbbd513
Nolan Donley <nolandonley14@gmail.com>
2021-06-04 23:13:15 -0400

    Updates

---
+++ precache-manifest.2e441c98971d07225ec6734192efe658.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca30d215545a11b939c2",
+    "url": "/tedTubeDemo/static/css/main.6134ae91.chunk.css"
+  },
+  {
+    "revision": "ca30d215545a11b939c2",
+    "url": "/tedTubeDemo/static/js/main.ca30d215.chunk.js"
+  },
+  {
+    "revision": "389b478fdb9efb4b3682",
+    "url": "/tedTubeDemo/static/js/1.389b478f.chunk.js"
+  },
+  {
+    "revision": "541a4c1036634cc671f8",
+    "url": "/tedTubeDemo/static/js/runtime~main.541a4c10.js"
+  },
+  {
+    "revision": "8f61c262fb014a653b2078c2091c3d4d",
+    "url": "/tedTubeDemo/index.html"
+  }
+];
